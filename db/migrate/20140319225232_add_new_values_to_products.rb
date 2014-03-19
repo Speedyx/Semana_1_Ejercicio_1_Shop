6ae0b7475f4a36e22cbd73574c104aadb5d879fd
Speedyx <cacope76@hotmail.com>
2014-03-19 23:56:37 +0100

    Tabla productos Ok
    El valor varchar en mysql es string en rails
    Si en rails damos de alta el campo como varchar entonces peta.

---
+++ db/migrate/20140319225232_add_new_values_to_products.rb
@@ -0,0 +1,5 @@
+class AddNewValuesToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313174858) do
+ActiveRecord::Schema.define(version: 20140319225232) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140313174858) do
     t.integer  "units"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
 end
