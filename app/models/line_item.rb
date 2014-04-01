63841dff5292499f81745d138733c99e588a8315
Speedyx <cacope76@hotmail.com>
2014-04-02 01:05:12 +0200

    Validaciones en line_item y order
    Estado no es state es status, y tenemos que cambiar la BD

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,11 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+    validates_presence_of :product_id
+    validates_presence_of :units
+    validates_numericality_of :units, :only_integer => true, :greater_than_or_equal_to => 1
+    validates_presence_of :price
+    validates_numericality_of :price, :greater_than_or_equal_to => 0
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ActiveRecord::Base
     has_many :line_items
     has_many :products, through: :line_items
     accepts_nested_attributes_for :line_items, :allow_destroy => true, :reject_if => lambda { |a| a[:product_id].blank? }
+
+    validates_presence_of :name
+    validates_presence_of :surname
+    validates_presence_of :address
+    validates_presence_of :city
+    validates_presence_of :payment
+    validates_format_of :payment, :with => /\A\b(efectivo|tarjeta|paypal)\z/, :message => "efectivo, tarjeta o paypal"
+    validates_presence_of :state
+    validates_format_of :state, :with => /\A\b(Pendiente|Enviado|Recibido)\z/, :message => "Pendiente, Enviado, Recibido"
+
 end
