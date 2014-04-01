8d7c8cb5f4edb787c0c21eae4e6681545d09c1e3
Speedyx <cacope76@hotmail.com>
2014-04-02 00:52:23 +0200

    Validaciones en user y product
    Error con los acentos en product, así que añadimos:
    #encoding: utf-8
    
    En config/application.rb no hace caso.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
+#encoding: utf-8
+
 class Product < ActiveRecord::Base
     has_many :line_items
     has_many :orders, through: :line_items
+
+    validates_presence_of :name
+    validates_uniqueness_of :name
+    validates_presence_of :added_at
+    validates_presence_of :units
+    validates_numericality_of :units, :only_integer => true, :greater_than_or_equal_to => 0
+    validates_presence_of :price
+    validates_numericality_of :price, :greater_than_or_equal_to => 0
+    validates_format_of :category, :with => /\A\b(libros|música & peliculas|electrónica|hogar|juguetes|ropa,
+comestibles)\z/, :allow_blank => true, :message => "libros, música & peliculas, electrónica, hogar, juguetes, ropa,
+comestibles, o estar vacía."
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,30 @@ class User < ActiveRecord::Base
     has_secure_password
     #attr_accessible :name, :email, :password, :password_confirmation
 
-    validates_presence_of :password, :on => :create
+    #Expresiones regulares en:
+    # http://rubular.com/
+
+    #validates_presence_of :password, :on => :create
+    validates_presence_of :password
+    # Validar password
+    # ^ => Empieza y tiene
+    # ?=.*[a-zA-Z] => Busca algún caracter que sea letra
+    # ?=.*[0-9] => Busca algún caracter que sea número
+    # {8,} => 8 de largo o más sin tope
+    validates_format_of :password, :with => /(?=.*[a-zA-Z])(?=.*[0-9]).{8,}/, :message => "8 characters at least including 1 number and 1 char."
     validates_presence_of :email
     validates_uniqueness_of :email
+    #Validar email
+    # [] => Especificación de rango
+    # \A y \z => Inicio y fin de cadena
+    # ^ y $ => Inicio y fin de línea
+    # \s => Algún caracter en blanco
+    # . => un carácter singular
+    # /\A[^@\s] => Empieza la cadena y no puede haber arroba  y espacio antes de la arroba
+    # @ => Y después de la arroba
+    # ([^@\s]+\.) => No puede haber arroba  y espacio después  de la arroba, y luego va un punto
+    # [^@\s]+\z/ => No puede haber arroba  y espacio después de la arroba, y luego va final de cadena
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,:message => "It seems to be invalid."
     validates_presence_of :name
     validates_uniqueness_of :name
 
