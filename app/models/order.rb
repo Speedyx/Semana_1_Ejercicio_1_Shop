d79fcaa7dcb7d9d7e509e76f33cd47776f75d293
Speedyx <cacope76@hotmail.com>
2014-04-16 02:27:02 +0200

    Desplegable con los métidos de pago y estado
    No están traducidos

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,19 @@ class Order < ActiveRecord::Base
 
     before_create :set_status
 
+      PAYMENTS = %w(Cash Card Paypal)
+      #PAYMENTS = %w(#{t('var.payments.cash')} #{t('var.payments.card')} #{t('var.payments.paypal')})
+      #PAYMENTS = %w(/\A\b(t('var.payments.cash')|t('var.payments.card')|t('var.payments.paypal'))\z/)
+      STATUS = %w(Waiting Sent Delivered)
+
     validates :name, :surname, :address, :city, :payment, presence: true
     validates :status, on: :update, presence: true
 
-    validates :payment, format: {with: /\A\b(Efectivo|Tarjeta|Paypal)\z/, message: "Efectivo, Tarjeta o Paypal"}
-    validates :status, on: :update, format: {with: /\A\b(Pendiente|Enviado|Recibido)\z/, message: "Pendiente, Enviado, Recibido"}
+    #validates :payment, format: {with: /\A\b(Efectivo|Tarjeta|Paypal)\z/, message: "Efectivo, Tarjeta o Paypal"}
+    #validates :status, on: :update, format: {with: /\A\b(Pendiente|Enviado|Recibido)\z/, message: "Pendiente, Enviado, Recibido"}
+    validates :payment, inclusion: { in: PAYMENTS, message: "%{value} is not a valid payment" }
+    validates :status, inclusion: { in: STATUS, message: "%{value} is not a valid state" }
+
 
     protected
         def set_status
