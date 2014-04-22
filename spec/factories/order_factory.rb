28f6ee0459c3937611e29283b5df28bc18418aa2
Speedyx <Speedyx@users.noreply.github.com>
2014-04-22 13:41:28 +0200

    Create order_factory.rb

---
+++ spec/factories/order_factory.rb
@@ -0,0 +1,13 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :order do
+    status "Waiting"
+    payment "Paypal"
+    address "My home"
+    city "My city"
+    name "My Name"
+    surname "My Surname"
+  end
+
+end
