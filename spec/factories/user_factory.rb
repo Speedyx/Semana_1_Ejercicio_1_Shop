6823f53f26ea9bddce4b8779cc9092f2a3ef2071
Speedyx <Speedyx@users.noreply.github.com>
2014-04-22 12:46:47 +0200

    user_factory

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,10 @@
+require 'faker'
+FactoryGirl.define do
+  factory :user do
+    name "MiNombre"
+    surname "Mis Apellidos"
+    email {Faker::Internet.email}
+    password "pass1234"
+    password_confirmation "pass1234"
+  end
+end
