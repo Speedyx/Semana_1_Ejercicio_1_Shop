85c23471038949700084c06962ad1c18627958fb
Speedyx <Speedyx@users.noreply.github.com>
2014-04-22 10:11:56 +0200

    Some product model tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,28 +2,22 @@ require 'spec_helper'
 
 describe Product do
 
-    before :each do
-    #before :all do
-        product = Product.new(:name => "Gadget", :description => "Some test description", :added_at => "2014-03-23 15:15:15", :manufacturer => "Samsung", :units => 5, :category => "Electronics", :price => 9.95)
-    end
+    #before :each do
+        #product = Product.new(:name => "Gadget", :description => "Some test description", :added_at => "2014-03-23 15:15:15", :manufacturer => "Samsung", :units => 5, :category => "Electronics", :price => 9.95)
+    #end
 
     describe "#new" do
-        #Funciona
         it "returns a new product object" do
-            product = Product.new(:name => "Gadget", :description => "Some test description", :added_at => "2014-03-23 15:15:15", :manufacturer => "Samsung", :units => 5, :category => "Electronics", :price => 9.95)
-            product.should be_an_instance_of Product
+            should be_an_instance_of Product
         end
 
-        #Funciona
         it "Is invalid" do
-            product = Product.new
-            product.should_not be_valid
+            should_not be_valid
         end
 
     end
 
     describe "#create" do
-        #Funciona
         it "change number or products" do
             product = Product.new(:name => "Gadget", :description => "Some test description", :added_at => "2014-03-23 15:15:15", :manufacturer => "Samsung", :units => 5, :category => "Electronics", :price => 9.95)
             expect {product.save}.to change {Product.count}.by(1)
@@ -31,12 +25,32 @@ describe Product do
 
     end
 
-    #No funciona
-    describe "#category" do
-        it "returns the correct category" do
-            @product.category.should eql :category
-        end
-
+    it "responds to name" do
+        should respond_to(:name)
+    end
+    it "responds to description" do
+        should respond_to(:description)
+    end
+    it "responds to added_at" do
+        should respond_to(:added_at)
+    end
+    it "responds to manufacturer" do
+        should respond_to(:manufacturer)
+    end
+    it "responds to units" do
+        should respond_to(:units)
+    end
+    it "responds to created_at" do
+        should respond_to(:created_at)
+    end
+    it "responds to category" do
+        should respond_to(:category)
+    end
+    it "responds to price" do
+        should respond_to(:price)
+    end
+    it "not responds to zzz" do
+        should_not respond_to(:zzz)
     end
 
 end
